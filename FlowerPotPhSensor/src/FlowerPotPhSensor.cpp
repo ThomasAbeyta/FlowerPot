d67a9be0fb4885e66c469c21bd18b0436afd4c1b
Tomas Abeyta <Abeyta@toms-macbook.admin.ad.cnm.edu>
2022-03-18 10:48:38 -0600

    starting code

---
+++ FlowerPotPhSensor/src/FlowerPotPhSensor.cpp
@@ -0,0 +1,27 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/Abeyta/Documents/IoT/TomsFlowerPot/FlowerPotPhSensor/src/FlowerPotPhSensor.ino"
+/*
+ * Project FlowerPotPhSensor
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "/Users/Abeyta/Documents/IoT/TomsFlowerPot/FlowerPotPhSensor/src/FlowerPotPhSensor.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
---
+++ FlowerPotPhSensor/src/FlowerPotPhSensor.ino
@@ -0,0 +1,128 @@
+/*
+ * Project L14_03_andAhalf_GPS_publish
+ * Description:
+ * Author: Thomas Abeyta
+ * Date: March 18 2022
+ */
+
+#include <Adafruit_MQTT.h>
+
+#include "Adafruit_MQTT/Adafruit_MQTT.h"
+#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
+#include "credentials.h"
+#include <JsonParserGeneratorRK.h>
+
+const int BUTTON = D7;
+
+TCPClient TheClient;
+
+Adafruit_MQTT_SPARK mqtt(&TheClient, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+Adafruit_MQTT_Publish mqttObj1 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/abq_gps");
+Adafruit_MQTT_Subscribe mqttON_OFFobject = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/ON_OFF");
+
+unsigned long last, lastTime;
+int value1, ON_OFF;
+struct geo {
+    float lat;
+    float lon;
+    int alt;
+    };
+
+geo myLoc;
+geo locations[13];
+
+SYSTEM_MODE(SEMI_AUTOMATIC);
+
+void setup() {
+    Serial.begin(9600);
+    waitFor(Serial.isConnected, 15000); // wait for Serial Monitor to startup
+
+    // Connect to WiFi without going to Particle Cloud
+    WiFi.connect();
+    while (WiFi.connecting()) {
+        Serial.printf(".");
+    }
+
+    // Setup MQTT subscription for onoff feed.
+    mqtt.subscribe(&mqttON_OFFobject);
+
+    pinMode(BUTTON, OUTPUT);
+
+    myLoc.lat = random(35.02171, 35.190231);
+    myLoc.lon = random(-106.65818, -106.515716);
+    myLoc.alt = random(1517, 1762);
+
+    Serial.printf("Location: lat %f, lon %f,  alt %i \n", myLoc.lat, myLoc.lon, myLoc.alt);
+}
+
+void createEventPayLoad(float lat, float lon, float alt) {
+    JsonWriterStatic<256> jw;
+    {
+        JsonWriterAutoObject obj(&jw);
+
+    jw.insertKeyValue("Temperature", lat);
+    jw.insertKeyValue("Pressure", lon); 
+    jw.insertKeyValue("Humidity", alt);
+    }
+    Particle.publish("abq_gps", jw.getBuffer(), PRIVATE);
+}
+
+void loop() {
+    // Validate connected to MQTT Broker
+    MQTT_connect();
+
+    // Ping MQTT Broker every 2 minutes to keep connection alive
+    if ((millis() - last) > 120000) {
+        Serial.printf("Pinging MQTT \n");
+        if (!mqtt.ping()) {
+            Serial.printf("Disconnecting \n");
+            mqtt.disconnect();
+        }
+        last = millis();
+    }
+
+    // publish to cloud every 30 seconds
+    
+    if((millis()-lastTime > 6000)) {
+      if(mqtt.Update()) {
+        mqttObj1.publish(value1);
+        Serial.printf("Publishing %0.2f \n",value1);
+        }
+      lastTime = millis();
+    }
+
+    // this is our 'wait for incoming subscription packets' busy subloop
+    Adafruit_MQTT_Subscribe *subscription;
+    while ((subscription = mqtt.readSubscription(100))) {
+        if (subscription == &mqttON_OFFobject) {
+            ON_OFF = atof((char *)mqttON_OFFobject.lastread);
+            Serial.printf("Received %i from Adafruit.io feed FeedNameB \n", ON_OFF);
+        }
+    }
+    if (ON_OFF == 1) {
+        digitalWrite(BUTTON, HIGH);
+    } else {
+        digitalWrite(D7, LOW);
+    }
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+void MQTT_connect() {
+    int8_t ret;
+
+    // Stop if already connected.
+    if (mqtt.connected()) {
+        return;
+    }
+
+    Serial.print("Connecting to MQTT... ");
+
+    while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+        Serial.printf("%s\n", (char *)mqtt.connectErrorString(ret));
+        Serial.printf("Retrying MQTT connection in 5 seconds..\n");
+        mqtt.disconnect();
+        delay(5000); // wait 5 seconds
+    }
+    Serial.printf("MQTT Connected!\n");
+}
